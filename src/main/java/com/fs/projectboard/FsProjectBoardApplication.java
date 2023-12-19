3a0d7e0105bbb9311aef1f0af33f4ce7f4fe235e
IronKim <tankokim@gmail.com>
2023-12-19 19:54:56 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링부트 프로젝트 초기화
    들어간 초기 기술은:
    
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/fs/projectboard/FsProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fs.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsProjectBoardApplication.class, args);
+    }
+
+}
