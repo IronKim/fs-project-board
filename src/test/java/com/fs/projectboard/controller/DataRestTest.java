389cd0706799a9811e251cdeb54b27661e932710
IronKim <tankokim@gmail.com>
2023-12-21 22:43:54 +0900

    #1 - data rest 테스트
    꼭 필요한 테스트는 아니지만, 공부 겸 작성
    data rest 테스트는 인테그레이션 테스트로 접근할 수 밖에 없다.
    
    DB 접근이 일어나므로 안전을 위해  `@Transactional` 적용
    테스트 안에서는 롤백 정책으로 동작한다.

---
+++ src/test/java/com/fs/projectboard/controller/DataRestTest.java
@@ -0,0 +1,91 @@
+package com.fs.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@WebMvcTest // @WebMvcTest는 스프링 MVC 테스트를 위한 어노테이션이다.
+@Transactional // @Transactional은 테스트 메소드가 실행된 후에 롤백을 수행한다.
+@DisplayName("Data REST - API 테스트")
+@AutoConfigureMockMvc // @AutoConfigureMockMvc는 MockMvc를 자동으로 설정해준다.
+@SpringBootTest // @SpringBootTest는 스프링 부트 테스트를 위한 어노테이션이다.
+public class DataRestTest {
+
+    private final MockMvc mvc; // MockMvc는 스프링 MVC 테스트를 위한 클래스이다.
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestingArticles_thenReturnsArticlesJsonResponse() throws Exception {
+        //Given
+
+        //When & Then
+        mvc.perform(get("/api/articles")) // GET /api/articles 요청을 보낸다.
+                .andExpect(status().isOk()) // 응답의 상태코드가 200인지 검증한다.
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json"))); // 응답의 Content-Type이 application/hal+json인지 검증한다.
+
+
+    }
+
+    @DisplayName("[api] 게시글 단건 조회")
+    @Test
+    void givenNothing_whenRequestingArticle_thenReturnsArticlesJsonResponse() throws Exception {
+        //Given
+
+        //When & Then
+        mvc.perform(get("/api/articles/1")) // GET /api/articles 요청을 보낸다.
+                .andExpect(status().isOk()) // 응답의 상태코드가 200인지 검증한다.
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json"))); // 응답의 Content-Type이 application/hal+json인지 검증한다.
+
+
+    }
+
+    @DisplayName("[api] 게시글 -> 댓글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestingArticleCommentsFromArticle_thenReturnsArticleCommentsJsonResponse() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articles/1/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestingArticleComments_thenReturnsArticleCommentsJsonResponse() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 단건 조회")
+    @Test
+    void givenNothing_whenRequestingArticleComment_thenReturnsArticleCommentJsonResponse() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articleComments/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+}
+
