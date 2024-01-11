cf17638bcd841586e06bfc2610257ddcbe3148b5
IronKim <tankokim@gmail.com>
2024-01-11 23:21:22 +0900

    #20 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현
    강의 시간 상 기능의 일부만 테스트로 표현함
    나머지 필요 기능은 별도 작성하여 넣기로.

---
+++ src/main/java/com/fs/projectboard/dto/ArticleCommentDto.java
@@ -0,0 +1,14 @@
+package com.fs.projectboard.dto;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO for {@link com.fs.projectboard.domain.ArticleComment}
+ */
+public record ArticleCommentDto(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy,
+                                String content) {
+    public static ArticleCommentDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt,
+                                       String modifiedBy, String content) {
+        return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+    }
+}
---
+++ src/main/java/com/fs/projectboard/service/ArticleCommentService.java
@@ -0,0 +1,27 @@
+package com.fs.projectboard.service;
+
+import com.fs.projectboard.dto.ArticleCommentDto;
+import com.fs.projectboard.repository.ArticleCommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleCommentRepository articleCommentRepository;
+    private final ArticleService articleService;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+}
