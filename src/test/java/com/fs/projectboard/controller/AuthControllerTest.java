82843d72c981f4e101d41d63aef4033bc35eecd8
IronKim <tankokim@gmail.com>
2024-01-20 19:05:56 +0900

    #47 - 문제가 된 테스트 코드 발견 및 수정
    전체 테스트를 통과하기 위한 코드 수정

--- src/test/java/com/fs/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/fs/projectboard/controller/AuthControllerTest.java
@@ -1,20 +1,25 @@
 package com.fs.projectboard.controller;
 
 import com.fs.projectboard.config.SecurityConfig;
+import com.fs.projectboard.config.TestSecurityConfig;
+import com.fs.projectboard.service.ArticleService;
+import com.fs.projectboard.service.PaginationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 인증")
-@Import(SecurityConfig.class) // @Import는 테스트에 필요한 설정 클래스를 임포트한다.
+@Import(TestSecurityConfig.class) // @Import는 테스트에 필요한 설정 클래스를 임포트한다.
 @WebMvcTest(Void.class) // @WebMvcTest는 MVC 테스트를 위한 클래스이다.
 public class AuthControllerTest {
 
@@ -24,6 +29,12 @@ public class AuthControllerTest {
         this.mvc = mvc;
     }
 
+    @MockBean
+    private ArticleService articleService;
+
+    @MockBean
+    private PaginationService paginationService;
+
     @DisplayName("[view][GET] 로그인 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenTryingToLogIn_thenReturnsLogInView() throws Exception {
@@ -32,7 +43,8 @@ public class AuthControllerTest {
         //When & Then
         mvc.perform(get("/login")) // GET /login 요청을 보낸다.
                 .andExpect(status().isOk()) // 응답의 상태코드가 200인지 검증한다.
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)) // 응답의 컨텐츠 타입이 text/html인지 검증한다.
-                .andDo(MockMvcResultHandlers.print()); // 응답을 출력한다.
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)); // 응답의 컨텐츠 타입이 text/html인지 검증한다.
+        then(articleService).shouldHaveNoInteractions();
+        then(paginationService).shouldHaveNoInteractions();
     }
 }
